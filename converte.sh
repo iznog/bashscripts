2975cef0c1518bfb71abdb8f89f7b2f96d366268
iznog <paulo.cruz@paulocruz.com.pt>
2016-12-15 17:49:55 +0000

    Update converte.sh
    Added description to script converte.sh

--- converte.sh
+++ converte.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Script to convert project development files from iso-8859-1 to utf8
+
+
 for i in $(ls -lh *.php | awk -F " " '{print $9}') 
 do
 	iconv -t utf8 -f iso-8859-1 $i -o $i.utf8
