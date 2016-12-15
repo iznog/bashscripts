2ec2e895930cb32fdeaf3e224f801de257b45034
iznog <paulo.cruz@paulocruz.com.pt>
2016-12-15 17:48:02 +0000

    Add files via upload

---
+++ converte.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+for i in $(ls -lh *.php | awk -F " " '{print $9}') 
+do
+	iconv -t utf8 -f iso-8859-1 $i -o $i.utf8
+done
+
+cd auth
+for i in $(ls -lh *.php | awk -F " " '{print $9}') 
+do
+	iconv -t utf8 -f iso-8859-1 $i -o $i.utf8
+done
+
+cd ../objects
+
+for i in $(ls -lh *.php | awk -F " " '{print $9}') 
+do
+	iconv -t utf8 -f iso-8859-1 $i -o $i.utf8
+done
+
+cd ../resources
+for i in $(ls -lh *.php | awk -F " " '{print $9}') 
+do
+	iconv -t utf8 -f iso-8859-1 $i -o $i.utf8
+done
+
+cd ..
+
