abc01cdf7690962727e4c59afa75fe0a9ddf29b6
Paulo Cruz <paulo.cruz@paulocruz.com.pt>
2017-02-03 00:24:09 +0000

    Commenting the file

--- installinsync.sh
+++ installinsync.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+
+# includes release information files and respective variables
 . /etc/lsb-release
 
-echo $DISTRIB_RELEASE
-
+# changing the distribution ID since ubuntu is on the lsb-release as "Ubuntu"
 DISTRIB_NAME=$(echo "$DISTRIB_ID" | tr '[:upper:]' '[:lower:]')
+
+# getting repository key
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
 
+# creating list file
 touch /etc/apt/sources.list.d/insynchq.list
 
+# getting repository information on list file
 echo "deb http://apt.insynchq.com/$DISTRIB_NAME $DISTRIB_CODENAME non-free contrib" >/etc/apt/sources.list.d/insynchq.list
 
+# updating package information
 apt-get update;
 
+# Finally installing insync
 apt-get install insync
